749621bc5807b86bce31827ad9e88f878c2d8ced
simeon <>
2003-01-14 19:31:09 +0000

    Release 14Jan2003 [Simeon]

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
 # Makefile for utf8conditioner
 # Simeon Warner - July 2001...
-# [CVS: $Id: Makefile,v 1.2 2001/08/01 22:03:12 simeon Exp $
+# [CVS: $Id: Makefile,v 1.3 2003/01/14 19:31:09 simeon Exp $
 
 OBJ = utf8conditioner.o getopt.o
 EXECUTABLE = utf8conditioner
 
+CC = gcc
 LIBS = 
 CFLAGS =
-CC = gcc
 
 all utf8conditioner: $(OBJ)
 	$(CC) $(OBJ) $(LIBS) -o $(EXECUTABLE)
@@ -16,10 +16,13 @@ strict:
 	glintc utf8conditioner.c getopt.c $(LIBS) -o $(EXECUTABLE)
 
 utf8conditioner.o: utf8conditioner.c getopt.h
-	$(CC) -c utf8conditioner.c
+	$(CC) $(CFLAGS) -c utf8conditioner.c
 
 getopt.o: getopt.c getopt.h
-	$(CC) -c getopt.c
+	$(CC) $(CFLAGS) -c getopt.c
+
+clean:
+	rm -f $(OBJ) $(EXECUTABLE)
 
 tar:
 	cd ..;tar -zcf /tmp/utf8.tar.gz utf8/utf8conditioner.c utf8/getopt.c utf8/getopt.h utf8/Makefile utf8/testfile; cd utf8
--- utf8conditioner.c
+++ utf8conditioner.c
@@ -1,5 +1,11 @@
 /* UTF-8 checker and conditioner
- * Simeon Warner - 10July2001... 
+ * Copyright (C) 2001-2003 Simeon Warner - simeon@cs.cornell.edu
+ *
+ * utf8conditioner is supplied under the GNU Public License and comes 
+ * with ABSOLUTELY NO WARRANTY; see COPYING for more details.
+ *
+ * utf8conditioner includes software developed by the University of
+ * California, Berkeley and its contributors. (getopt)
  *
  * Designed for use with Open Archives Initiative (OAI, 
  * see: http://www.openarchives.org/) harvesting software. Aims to 
@@ -14,7 +20,7 @@
  * valid single byte will be written out, invalid single bytes will be 
  * replaced with a dummy character. The hope is that this will avoid an 
  * error having run-on effects which might interfere with XML markup.
- * (Option -m changes this behaviour to replace with invalid multi-byte 
+ * (Option -m changes this behaviour to replace with invalid multi-byte
  * with a single dummy character.)
  *
  * Bugs:
@@ -22,8 +28,33 @@
  *   (unsigned long int). Will result in incorrect output messages.
  * - doesn't handle UTF-8 encoding of UTF-16/UCS-4. 
  *
- * [CVS: $Id: utf8conditioner.c,v 1.8 2002/08/08 04:33:43 simeon Exp $]
+ * [CVS: $Id: utf8conditioner.c,v 1.9 2003/01/14 19:31:09 simeon Exp $]
  */
+
+#define PROGRAM_NOTICE "\
+utf8conditioner version 14Jan2003. Copyright (C) 2001-2003 Simeon Warner\n\
+\n\
+utf8conditioner is supplied under the GNU Public License and comes\n\
+with ABSOLUTELY NO WARRANTY; run with -L flag for more details.\n\
+utf8conditioner includes software developed by the University of\n\
+California, Berkeley and its contributors. (getopt)\n"
+
+#define GNU_GPL_NOTICE1 "\
+This program is free software; you can redistribute it and/or modify\n\
+it under the terms of the GNU General Public License as published by\n\
+the Free Software Foundation; either version 2 of the License, or\n\
+(at your option) any later version.\n\n"
+
+#define GNU_GPL_NOTICE2 "\
+This program is distributed in the hope that it will be useful,\n\
+but WITHOUT ANY WARRANTY; without even the implied warranty of\n\
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the\n\
+GNU General Public License for more details.\n\
+\n\
+You should have received a copy of the GNU General Public License\n\
+along with this program (in the file COPYING); if not, visit\n\
+http://www.gnu.org/licenses/gpl.html or write to the Free Software\n\
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111 USA\n"
 
 #include <stdio.h>
 extern int snprintf(char *str, size_t size, const char *format, ...);
@@ -34,11 +65,12 @@ extern int snprintf(char *str, size_t size, const char *format, ...);
 #define MAX_BAD_CHAR 100
 
 extern const char *optarg;
+extern const int optind;
 extern int getopt (int argc, char *const *argv, const char *shortopts);
 
 int validUnicodeChar(unsigned int ch);
 int validXMLChar(unsigned int ch);
-
+int validUTF8Char(unsigned int ch);
 
 int main (int argc, char* argv[]) {
   int j,k;
@@ -53,7 +85,7 @@ int main (int argc, char* argv[]) {
   unsigned long int linenum=1;    /* count of lines */
   unsigned int unicode;           /* Unicode character represented by UTF-8 */
  
-  int maxErrors=100;              /* max number of error messages to print */
+  int maxErrors=1000;             /* max number of error messages to print */
   int numErrors=0;                /* count of errors */   
   int quiet=0;                    /* quiet option */
   int check=0;                    /* check only option */
@@ -78,28 +110,34 @@ int main (int argc, char* argv[]) {
   /*
    * Read any options
    */
-  while ((j=getopt(argc,argv,"hqce:b:s:xm"))!=EOF) {
+  while ((j=getopt(argc,argv,"hH?qce:b:s:xmlL"))!=EOF) {
     switch (j) {
-      case '?':
       case 'h': 
+      case 'H':
+      case '?':
         /* string split to meet ISO C89 requirement of <=509 chars */
-        fprintf(stderr,
-          "usage: %s [-q] [-c] [-e num] [[-b char]] [-x] [-s char] [-h]\n"
-          "Takes UTF-8 input from stdin, writes processed UTF-8 to stdout\n"
-	  "and errors/warnings to stderr.\n\n", argv[0]);
-        fprintf(stderr,
-          "  -q   quiet, no output messages to stderr\n"
-          "  -c   just check, no output of XML to stdout\n"   
-          "  -e   number of error messages to print (currently %d, 0 for unlimited)\n"
-          "  -b   add Unicode character code to list of bad codes (decimal, 0octal, 0xhex),\n"
-  	  "       use multiple times at add multiple characters\n", maxErrors);
-        fprintf(stderr,
-          "  -s   change character substituted for bad codes (currently '%c')\n"
-          "  -m   replace invalid multi-byte sequence with multiple dummy characters\n"
-          "  -x   remove codes not allowed in UTF-8 XML streams\n"
-          "       (valid: #x9 | #xA | #xD | [#x20-#xD7FF] | [#xE000-#xFFFD] | [#x10000-#x10FFFF])\n"
-          "  -l   lax - don't check for overlong encodings\n"
-          "  -h   this help\n\n", substituteChar);  
+        fprintf(stderr, PROGRAM_NOTICE);
+        fprintf(stderr,"\n\
+usage: %s [-q] [-c] [-e num] [[-b char]] [-x] [-s char] [-h]\n\n\
+Takes UTF-8 input from stdin, writes processed UTF-8 to stdout\n\
+and errors/warnings to stderr.\n\n", argv[0]);
+        fprintf(stderr,"\
+  -c   just check, no output of XML to stdout\n\
+  -q   quiet, don't output messages to stderr\n\
+  -x   XML check, remove codes not allowed in UTF-8 XML streams\n\
+       (valid codes are: #x9 | #xA | #xD and the ranges\n\
+        [#x20-#xD7FF] | [#xE000-#xFFFD] | [#x10000-#x10FFFF])\n\n");
+        fprintf(stderr,"\
+  -b   add Unicode character code to list of bad codes\n\
+       (specified as decimal, 0octal or  0xhex),\n\
+       use multiple times at add multiple characters\n\
+  -e   maximum number of error messages to print\n\
+       (currently %d, 0 for unlimited)\n\
+  -l   lax - don't check for overlong encodings\n\
+  -m   replace invalid multi-byte sequences with multiple dummy characters\n\
+  -s   change character substituted for bad codes (currently '%c')\n\n\
+  -L   display information about license\n\
+  -h   this help\n\n", maxErrors, substituteChar);  
         exit(1);
       case 'q':
         quiet=1;
@@ -109,11 +147,11 @@ int main (int argc, char* argv[]) {
         break; 
       case 'b':
         if (badChar>=(MAX_BAD_CHAR-1)) {
-          fprintf(stderr,"Too many bad codes specified, limit %d.\n", MAX_BAD_CHAR);
+          fprintf(stderr,"Too many bad codes specified (limit %d), aborting!\n", MAX_BAD_CHAR);
           exit(1); 
         } 
         badChars[badChar++]=(int)strtoul(optarg,NULL,0);
-        badChars[badChar]='\0';
+        badChars[badChar]=0;
         break;
       case 'e':
         maxErrors=(int)strtoul(optarg,NULL,0);
@@ -130,7 +168,20 @@ int main (int argc, char* argv[]) {
       case 'x':
         checkXMLChars=1;
         break; 
+      case 'L':
+        fprintf(stderr,GNU_GPL_NOTICE1);
+        fprintf(stderr,GNU_GPL_NOTICE2);
+        exit(1);
     }
+  }
+
+  /*
+   * Barf if anything on command line left unread (probably an attempt to 
+   * specify a file name instead of using stdin)
+   */
+  if (argc>optind) {
+    fprintf(stderr,"Unknown parameters specified on command line (-h for help), aborting!\n");
+    exit(1); 
   }
 
   /*
@@ -198,11 +249,10 @@ int main (int argc, char* argv[]) {
     }
 
     if (error[0]=='\0') {
-      k=-1;
       if (checkXMLChars && !validXMLChar(unicode)) {
         snprintf(error,sizeof(error),"code not allowed in XML: 0x%04X",unicode);
       } else { 
-        while (badChars[++k]!=0) {
+        for (k=0; badChars[k]!=0; k++) {
           if (unicode==badChars[k]) {
             snprintf(error,sizeof(error),"bad code: 0x%04X", unicode);
             break;
@@ -220,7 +270,6 @@ int main (int argc, char* argv[]) {
     /* check for overlong encodings if no error already */
     if ((error[0]=='\0') && checkOverlong && contBytes>0 
                          && (unicode<=highestCharInNBytes[contBytes-1])) {
-      unicode=substituteChar;
       snprintf(buf,sizeof(buf),"overlong encoding, not safe: 0x%04X",unicode);
       strncat(error,buf,sizeof(error));
     }
@@ -256,16 +305,14 @@ int main (int argc, char* argv[]) {
         fprintf(stderr,"Line %ld, char %ld, byte %ld: %s\n",
                 linenum,charnum,bytenum,error);
       }
-      if (!check) { 
-        for (k=0; k<=(j-1); k++) {
-          putc(byte[k],stdout);
-        }
+      contBytes=j-1;
+    }
+
+    if (!check) { 
+      for (k=0; k<=contBytes; k++) {
+        putc(byte[k],stdout);
       }
-    } else {
-      for (j=0; j<=contBytes; j++) {
-        if (!check) { putc(byte[j],stdout); }
-      }
-    }   
+    }
   }
 
   if (!quiet && (numErrors>maxErrors) && (maxErrors!=0)) {
